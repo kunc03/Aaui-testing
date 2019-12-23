5c0e65ffe0352d35a99df3b7b12e77c492efb9fd
inovasi solusi <fikran.jabbar.pasha@gmail.com>
2019-12-23 17:03:47 +0700

    New Route

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+    // Import For Header SideBar
+    Header			: require('./Header_sidebar/Header').default,
+    Loader			: require('./Header_sidebar/Loader').default,
+    SideBar			: require('./Header_sidebar/Sidebar').default,
+
+    // import component
+    Login 			: require('./Login').default,
+    Logout 			: require('./Logout').default,
+    LoginVoucher	: require('./Login/index-v').default,
+    Home			: require('./Home').default,
+    Pengaturan      : require('./Pengaturan').default,
+    Profile	        : require('./Profile').default,
+	//Page404			: require('./404').default,
+    
+    // Import Users
+	Users	        : require('./Users/User').default,
+    UserAdd	        : require('./Users/User/add').default,
+    UserCabang      : require('./Users/UserCabang').default,
+    UserGroup       : require('./Users/UserGroup').default,
+}
+
+
+/**
+ * Login 
+ * jek@jek.com
+ * 123456
+ */
