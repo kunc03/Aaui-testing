ecb08f40af549980dd1721bef2d7c15878ad899e
inovasi solusi <fikran.jabbar.pasha@gmail.com>
2020-02-06 15:15:23 +0700

    .

--- src/App.js
+++ src/App.js
@@ -61,73 +61,95 @@ export default class App extends React.Component {
 
   componentDidMount() {
     let userInfo = localStorage.getItem("user");
-
-
-    // if (userInfo == null) {
-    //   this.setState({ userLogin: false });
-    // } else {
-    //   this.setState({ userLogin: true });
-    // }
-    
-  }
-
-  _privateRoute() {
-    // let workSpace = null;
-    // if (this.state.userLogin) {
-    //   workSpace = <Main />;
-    // } else {
-    //   workSpace = <Login />;
-    // }
-
-    // return <div>{workSpace}</div>;
+    if (userInfo == null) {
+      this.setState({ userLogin: false });
+    } else {
+      this.setState({ userLogin: true });
+    }
   }
 
   render() {
+    let workSpace = null;
+    if (this.state.userLogin) {
+      workSpace = <Main />;
+    } else {
+      workSpace = <Login />;
+    }
+
+    return <div>{workSpace}</div>;
+  }
+}
+
+export class Main extends React.Component {
+  state = {
+    level: Storage.get('user').data.level
+  }
+
+  render() {
+    let workSpaceSwitch = null;
+    if(this.state.level === 'superadmin') {
+      workSpaceSwitch = <SuperAdminSwitch />;
+    } else if(this.state.level === 'admin') {
+      workSpaceSwitch = <AdminSwitch />;
+    } else {
+      workSpaceSwitch = <ClientSwitch />;
+    }
+
     return (
-      <Router>
-        <div>
-          {userInfo ? 
-            <div>
-              <Loader />
-              <SideBar />
-              <Header />
-            </div>
-          : null }
-          
-          <Switch>
-                <Route
-									exact path={'/'}
-									render={() => <Redirect to={userInfo !== null ? '/home' : '/login'} />} />
-								
-								<Route
-									path={userInfo !== null ? '/home' :'/login'}
-									component={userInfo !== null ? Home : Login} />
-                
-                <Route
-									path={'/login-voucher'}
-									component={LoginVoucher} />
+      <div>
+        <Loader />
+        <Sidebar />
+        <Header />
+        {workSpaceSwitch}
+      </div>
+    );
+  }
+}
 
-                  {router.map((value, index) => {
-                      return (
-                          <PrivateRoute key={index} path={value.path} component={value.component} />
-                        )}
-                      ) 
-                  }
+export class Logout extends React.Component {
+  constructor(props) {
+    super(props);
 
-                  
-            {/* <Route path="/" exact component={Home} />
-            <Route path="/Pengaturan" exact component={Pengaturan} />
-            <Route path="/Profile" exact component={Profile} />
-            <Route path="/user" exact component={User} />
-            <Route path="/user-create" exact component={UserAdd} />
-            <Route path="/cabang" exact component={UserCabang} />
-            <Route path="/grup" exact component={UserGrup} />
-            <Route path="/logout" component={Logout} /> */}
+    this.onClickLogout = this.onClickLogout.bind(this);
+  }
 
-            {/* <Route path="/login" exact component={Login} /> */}
-          </Switch>
-        </div>
-      </Router>
+  onClickLogout(e) {
+    e.preventDefault();
+  }
+
+  componentDidMount() {
+    localStorage.clear();
+    window.location.href = window.location.origin;
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export class SuperAdminSwitch extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/pengaturan" component={Pengaturan} />
+        <Route path="/profile" component={Profile} />
+
+        <Route path="/user" component={User} />
+        <Route path="/user-create" component={UserAdd} />
+        <Route path="/user-edit/:user_id" component={UserEdit} />
+        
+        <Route path="/user-access" component={UserAccess} />
+        <Route path="/user-company/:company_id" component={UserCompany} />
+        
+        <Route path="/cabang" component={Cabang} />
+        <Route path="/company" component={Company} />
+        <Route path="/company-detail/:company_id" component={CompanyDetail} />
+        <Route path="/company-detail-super/:company_id" component={CompanyDetailSuper} />
+        <Route path="/grup" component={Grup} />
+
+        <Route path="/logout" component={Logout} />
+      </Switch>
     );
   }
 }
