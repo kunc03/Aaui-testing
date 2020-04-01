42b82d09a9a0c30e45b7821e6031ccfaf59bcfca
alvinkurniakamal <alvinkurniakamal@gmail.com>
2020-04-01 13:22:53 +0700

    hide collapse menu mobile

--- src/components/Header_sidebar/Header/index.js
+++ src/components/Header_sidebar/Header/index.js
@@ -46,14 +46,47 @@ class Header extends Component {
   }
 
   render() {
+    let levelUser = Storage.get('user').data.level;
+    let menuClients = [
+      { iconOn: 'dashboardon.png', iconOff: 'dashboardoff.png', label: 'Dashboard', link: '/' },
+      { iconOn: 'aktivitason.png', iconOff: 'aktivitasoff.png', label: 'Aktivitas', link: '/aktivitas' },
+      { iconOn: 'materion.png', iconOff: 'materioff.png', label: 'Kursus & Materi', link: '/kursus' },
+      { iconOn: 'diskusion.png', iconOff: 'diskusioff.png', label: 'Forum', link: '/forum' },
+      { iconOn: 'kelason.png', iconOff: 'kelasoff.png', label: 'Group Meeting', link: '/liveclass' },
+      { iconOn: 'profileon.png', iconOff: 'profileoff.png', label: 'Profile', link: '/profile' },
+      { iconOn: 'pengaturanon.png', iconOff: 'pengaturanoff.png', label: 'Pengaturan', link: '/pengaturan' },
+    ];
+
+    let menuAdmins = [
+      { iconOn: 'dashboardon.png', iconOff: 'dashboardoff.png', label: 'Dashboard', link: '/' },
+      { iconOn: 'mycompanyon.png', iconOff: 'mycompanyoff.png', label: 'My Company', link: '/my-company' },
+      { iconOn: 'materion.png', iconOff: 'materioff.png', label: 'Kursus & Materi', link: '/kursus-materi' },
+      { iconOn: 'userson.png', iconOff: 'usersoff.png', label: 'Users', link: '/user-company' },
+      { iconOn: 'accesson.png', iconOff: 'accessoff.png', label: 'Access', link: '/user-access' },
+      { iconOn: 'profileon.png', iconOff: 'profileoff.png', label: 'Profile', link: '/profile' },
+      { iconOn: 'pengaturanon.png', iconOff: 'pengaturanoff.png', label: 'Pengaturan', link: '/pengaturan' },
+    ];
+
+    let menuSuperAdmins = [
+      { iconOn: 'dashboardon.png', iconOff: 'dashboardoff.png', label: 'Dashboard', link: '/' },
+      { iconOn: 'foron.png', iconOff: 'foroff.png', label: 'Forum', link: '/forum' },
+      { iconOn: 'kelason.png', iconOff: 'kelasoff.png', label: 'Kelas', link: '/liveclass' },
+      { iconOn: 'userson.png', iconOff: 'usersoff.png', label: 'Users', link: '/user' },
+      { iconOn: 'companyon.png', iconOff: 'companyoff.png', label: 'Company', link: '/company' },
+      { iconOn: 'accesson.png', iconOff: 'accessoff.png', label: 'Access', link: '/user-access' },
+      { iconOn: 'profileon.png', iconOff: 'profileoff.png', label: 'Profile', link: '/profile' },
+      { iconOn: 'pengaturanon.png', iconOff: 'pengaturanoff.png', label: 'Pengaturan', link: '/pengaturan' },
+    ];
+  
+    let menuContent = [];
     const { user, level } = this.state;
     return (
       <header className="navbar pcoded-header navbar-expand-lg navbar-light">
         <div className="m-header">
-          <a className="mobile-menu" id="mobile-collapse1" href="javascript:">
+          {/* <a className="mobile-menu" id="mobile-collapse1" href="javascript:">
             <span />
-          </a>
-          <a href="index.html" className="b-brand">
+          </a> */}
+          <a href="/" className="b-brand">
             <div className="b-bg">
               <img
                 src="assets/images/component/logo-mobile.png"
