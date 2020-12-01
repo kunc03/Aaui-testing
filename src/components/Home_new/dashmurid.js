3ae2e74514415b2e55fc76eb6f501380ed4b2818
ahmad ardiansyah <ahmad.ardiansyah@carsworld.co.id>
2020-12-01 20:53:14 +0700

    dashboard murid

--- src/components/Home_new/dashmurid.js
+++ src/components/Home_new/dashmurid.js
@@ -7,6 +7,9 @@ import Storage from '../../repository/storage';
 import CalenderNew from '../kalender/kalender';
 import ListToDoNew from './listToDo';
 
+import { toast } from 'react-toastify'
+import moment from 'moment-timezone'
+
 import Pengumuman from '../Pengumuman/pengumuman';
 
 class DashMurid extends Component {
@@ -22,21 +25,23 @@ class DashMurid extends Component {
     pengumumanIsi: '',
     pengumumanFile: [],
 
-    tugas: [
-      {id: 1, mapel: 'Matematika', batas: '24 Des 2020', terkumpul: '20/30'},
-      {id: 2, mapel: 'Fisika', batas: '25 Des 2020', terkumpul: '29/30'},
-    ],
+    tugas: [],
 
-    ujian: [
-      {id: 1, tanggal: '24 Des 2020', waktu: '07:00', mapel: 'Matematika', durasi: '40 Menit'},
-      {id: 2, tanggal: '25 Des 2020', waktu: '08:00', mapel: 'Fisika', durasi: '45 Menit'},
-      {id: 3, tanggal: '29 Des 2020', waktu: '09:00', mapel: 'Biologi', durasi: '60 Menit'},
-    ],
+    ujian: [],
 
-    jadwal: [
-      {id: 1, mapel: 'Matematika', topik: 'Aljabar', waktu: '07:00 - 09:00', sesi: '2'},
-      {id: 2, mapel: 'Ilmu Pengetahuan Sosial', topik: 'Pancasila', waktu: '07:00 - 09:00', sesi: '3'},
-    ],
+    jadwal: [],
+  }
+
+  fetchJadwal() {
+    API.get(`${API_SERVER}v2/jadwal-mengajar/murid/${Storage.get('user').data.user_id}`).then(res => {
+      if(res.data.error) toast.warning(`Error: fetch jadwal`)
+
+      this.setState({
+        jadwal: res.data.result.jadwal,
+        tugas: res.data.result.tugas,
+        ujian: res.data.result.ujian,
+      })
+    })
   }
 
   openPengumuman = e => {
@@ -62,6 +67,7 @@ class DashMurid extends Component {
 
   componentDidMount() {
     this.fetchPengumuman();
+    this.fetchJadwal();
   }
 
   fetchPengumuman() {
@@ -80,6 +86,9 @@ class DashMurid extends Component {
   }
 
   render() {
+
+    console.log('state: ', this.state)
+
     return (
       <div className="pcoded-main-container" style={{ backgroundColor: "#F6F6FD" }}>
         <div className="pcoded-wrapper">
@@ -97,16 +106,16 @@ class DashMurid extends Component {
                           <table className="table table-striped">
                             <thead>
                               <tr>
-                                <th>Mata Pelajaran</th><th>Topik</th><th>Waktu</th><th>Sesi</th><th>Aksi</th>
+                                <th>Mata Pelajaran</th><th>Hari</th><th>Waktu</th><th>Sesi</th><th>Aksi</th>
                               </tr>
                             </thead>
                             <tbody>
                               {
                                 this.state.jadwal.map((item,i) => (
                                   <tr key={i} style={{borderBottom: '1px solid #e9e9e9'}}>
-                                    <td>{item.mapel}</td>
-                                    <td>{item.topik}</td>
-                                    <td>{item.waktu}</td>
+                                    <td>{item.nama_pelajaran}</td>
+                                    <td>{item.hari}</td>
+                                    <td>{item.jam_mulai}</td>
                                     <td>{item.sesi}</td>
                                     <td><i style={{cursor: 'pointer'}} className="fa fa-search"></i></td>
                                   </tr>
@@ -125,16 +134,16 @@ class DashMurid extends Component {
                           <table className="table table-striped">
                             <thead>
                               <tr>
-                                <th>Mata Pelajaran</th><th>Batas Waktu</th><th>Terkumpul</th><th>Aksi</th>
+                                <th>Tugas</th><th>Batas Waktu</th><th>Mata Pelajaran</th><th>Aksi</th>
                               </tr>
                             </thead>
                             <tbody>
                               {
                                 this.state.tugas.map((item,i) => (
                                   <tr key={i} style={{borderBottom: '1px solid #e9e9e9'}}>
-                                    <td>{item.mapel}</td>
-                                    <td>{item.batas}</td>
-                                    <td>{item.terkumpul}</td>
+                                    <td>{item.title}</td>
+                                    <td>{moment(item.time_finish).format('DD-MM-YYYY')}</td>
+                                    <td>{item.nama_pelajaran}</td>
                                     <td><i style={{cursor: 'pointer'}} className="fa fa-search"></i></td>
                                   </tr>
                                 ))
@@ -230,18 +239,17 @@ class DashMurid extends Component {
                           <table className="table table-striped">
                             <thead>
                               <tr>
-                                <th>Tanggal</th><th>Waktu</th><th>Mata Pelajaran</th><th>Durasi</th><th>Aksi</th>
+                                <th>Tanggal</th><th>Ujian</th><th>Mata Pelajaran</th><th>Aksi</th>
                               </tr>
                             </thead>
                             <tbody>
                               {
                                 this.state.ujian.map((item,i) => (
                                   <tr key={i}>
-                                    <td>{item.tanggal}</td>
-                                    <td>{item.waktu}</td>
-                                    <td>{item.mapel}</td>
-                                    <td>{item.durasi}</td>
-                                    <td><i style={{cursor: 'pointer'}} className="fa fa-search"></i></td>
+                                  <td>{moment(item.time_finish).format('DD-MM-YYYY')}</td>
+                                  <td>{item.title}</td>
+                                  <td>{item.nama_pelajaran}</td>
+                                  <td><i style={{cursor: 'pointer'}} className="fa fa-search"></i></td>
                                   </tr>
                                 ))
                               }
