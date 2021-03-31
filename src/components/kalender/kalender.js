bb46ec27f64dcebb0c2db0233ade1c076a1e7a4a
fikranjabbar13 <fikran.jabbar.pasha@gmail.com>
2021-03-31 10:41:16 +0700

    Calnder fullscreen

--- src/components/kalender/kalender.js
+++ src/components/kalender/kalender.js
@@ -2,7 +2,11 @@ import React, { Component, useState } from "react";
 import { Link } from "react-router-dom";
 import Storage from '../../repository/storage';
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
+import MinCalender from 'react-calendar';
+
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-calendar/dist/Calendar.css';
+
 import moment from 'moment';
 import { dataKalender } from '../../modul/data';
 import API, { USER_ME, API_SERVER } from '../../repository/api';
@@ -124,32 +128,69 @@ class KalenderNew extends Component {
             Webinar
           </div>
         </div>
+        {/* FULL SCRENN CALENDER */}
         <ReactFullScreenElement
           fullScreen={this.state.fullscreen}
           allowScrollbar={false}
         >
-          <div className="card p-10 hidden">
-            <h3 className="f-w-900 f-18 fc-blue">Calendar</h3>
-            <div style={{ position: 'absolute', top: 10, right: this.state.fullscreen ? 30 : 10 }}>
-              <i onClick={() => this.setState({ fullscreen: !this.state.fullscreen })} className={this.state.fullscreen ? 'fa fa-compress' : 'fa fa-expand'} style={{ marginRight: '0px !important', fontSize: '20px', cursor: 'pointer' }}></i>
+          <div className={this.state.fullscreen ? "card" : "hidden"}>
+            <div className="p-20" style={{ background: '#f3f3f3' }}>
+              <div className="f-w-900 f-18 fc-blue">
+                <button className="btn btn-icademy-primary">New Event</button>
+                <span className="float-right p-10">
+                  <span><i className="fa fa-share"></i> Share &nbsp; </span>
+                  <span><i className="fa fa-print"></i> Print &nbsp; </span>
+                  <i onClick={() => this.setState({ fullscreen: !this.state.fullscreen })} className='fa fa-compress ' style={{ marginRight: '0px !important', fontSize: '20px', cursor: 'pointer' }}></i>
+                </span>
+              </div>
             </div>
-            <Calendar
-              popup
-              events={event}
-              // defaultDate={new Date()}
-              localizer={localizer}
-              style={{ height: 400 }}
-              eventPropGetter={(event, start, end, isSelected) => {
-                if (event.bgColor) {
-                  return {
-                    style: { backgroundColor: event.type === 3 ? '#0091FF' : '#e2890d' },
-                  };
-                }
-                return {};
-              }}
-              views={['month', 'week', 'day', 'agenda']}
-              components={{ event: Event }}
-            />
+
+            <div className="row">
+              <div className="col-sm-2" style={{ background: '#f3f3f3' }}>
+                <MinCalender
+
+                />
+
+                <div className="fc-skyblue p-10"> <i className="fa fa-plus"></i> <b>Add Calendar</b></div>
+
+                <div className="p-10 "> <i className="fa fa-dropdown"></i> <b>My Calender</b></div>
+
+                <div className="p-10">
+                  <p><input type="radio"></input> calendar </p>
+                  <input type="radio"></input> Unitade Stade
+                  </div>
+
+              </div>
+              <div className="col-sm-7">
+                <Calendar
+                  popup
+                  events={event}
+                  // defaultDate={new Date()}
+                  localizer={localizer}
+                  style={{ height: '100%' }}
+                  eventPropGetter={(event, start, end, isSelected) => {
+                    if (event.bgColor) {
+                      return {
+                        style: { backgroundColor: event.type === 3 ? '#0091FF' : '#e2890d' },
+                      };
+                    }
+                    return {};
+                  }}
+                  views={['month', 'week', 'day', 'agenda']}
+                  components={{ event: Event }}
+                />
+              </div>
+              <div className="col-sm-3 borderLeftCalender">
+                <div className="p-10" >
+                  <span className="f-w-900 f-18 fc-grey ">Fri, Feb 19</span>
+                  <span className=" f-14 float-right">40</span>
+                </div>
+                <div className="" style={{ textAlign: 'center', marginTop: '15vh' }}>
+                  <h4>Nothing Planed For Today </h4>
+                </div>
+              </div>
+            </div>
+
             <div className="p-l-20 m-t-10">
               <span className="p-r-5" style={{ color: '#0091FF' }}>
                 <i className="fa fa-square"></i>
