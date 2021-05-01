f4318ba36a0717370df3c62c7c7220bdd0e8a023
Alvin <alvinkurniakamal@gmail.com>
2021-05-01 18:07:30 +0700

    generate training certificate

--- src/components/training/report/index.js
+++ src/components/training/report/index.js
@@ -13,6 +13,7 @@ import Storage from '../../../repository/storage';
 import DatePicker from "react-datepicker";
 import { MultiSelect } from 'react-sm-select';
 import Moment from 'moment-timezone';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 class Report extends Component {
   constructor(props) {
@@ -31,13 +32,41 @@ class Report extends Component {
       valueCompany: [],
       optionsLicensesType: [],
       valueLicensesType: [],
-      user: ''
+      user: '',
+      checkAll: false,
+      isModalSertifikat: false,
+      sertifikat: [],
+      companyLogo: '',
+      cert_logo: '',
+      cert_title: 'CERTIFICATE OF COMPLETION',
+      cert_subtitle: 'THIS CERTIFICATE IS PROUDLY PRESENTED TO',
+      cert_description: 'FOR SUCCESSFULLY COMPLETING ALL CONTENTS ON',
+      cert_sign_name: ''
     };
     this.goBack = this.goBack.bind(this);
   }
   
   goBack() {
     this.props.history.goBack();
+  }
+
+  fetchCompanyInfo() {
+    API.get(`${API_SERVER}v1/company/${this.state.companyId}`).then(res => {
+      if (res.data.error)
+        toast.warning("Error fetch API")
+      else
+        this.setState({ companyLogo: res.data.result.logo })
+    })
+  }
+
+  modalSertifikat() {
+    if (this.state.data.filter((item) => item.checked === true).length > 0) {
+      this.fetchCompanyInfo();
+      this.setState({ isModalSertifikat: true });
+    }
+    else {
+      toast.warning('Please select the data first')
+    }
   }
 
   handleChangeFile = e => {
@@ -59,10 +88,79 @@ class Report extends Component {
     this.setState({ filter: e.target.value });
   }
 
-  handleChange = (name, e) => {
+  sertifikat = () => {
+    if (this.state.cert_sign_name === '' || (this.state.signature === '' || this.state.signature === null)) {
+      toast.warning('Signature name and signature image is mandatory')
+    }
+    else {
+      let items = this.state.data;
+      let sertifikat = items.filter(e => { return e.checked }).map(e => {
+        return {
+          result_id: e.id,
+          company_id: this.state.companyId,
+          nama: e.name,
+          email: e.email,
+          cert_topic: e.course_name ? e.exam_name + ' - ' + e.course_name : e.exam_name
+        }
+      });
+
+      let formData = new FormData();
+      formData.append('company_id', this.state.companyId);
+      formData.append('cert_title', this.state.cert_title);
+      formData.append('cert_subtitle', this.state.cert_subtitle);
+      formData.append('cert_description', this.state.cert_description);
+      formData.append('cert_logo', this.state.cert_logo);
+      formData.append('signature', this.state.signature);
+      formData.append('cert_sign_name', this.state.cert_sign_name);
+      formData.append('data', JSON.stringify(sertifikat));
+
+      API.post(`${API_SERVER}v2/training/certificate`, formData).then(async (res) => {
+        toast.success(`Sending certificate to participant's email`);
+        this.handleModal();
+      });
+    }
+  }
+
+  handleModal = () => {
+    this.setState({
+      isModalSertifikat: false
+    });
+  }
+
+  handleChangeFilter = (name, e) => {
     this.setState({[name]: e}, () => {
         this.getList();
     })
+  }
+  handleChange = (e) => {
+    if (e.target.files[0]) {
+      if (e.target.files[0].size <= 5000000) {
+        this.setState({
+          [e.target.id]: e.target.files[0],
+          [`${e.target.id}_img`]: URL.createObjectURL(e.target.files[0]),
+        });
+      } else {
+        e.target.value = null;
+        toast.warning('Maximum file is 5MB')
+      }
+    }
+  };
+  handleChangeText = (e) => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      let a = this.state;
+      this.setState(a);
+    });
+  };
+
+  handleChangeChecked(e, item) {
+    item['checked'] = e.target.checked;
+  }
+  checkAll(e) {
+    let item = this.state.data
+    item.map((item, index) => {
+      item.checked = e.target.checked;
+    })
+    this.setState({ item, checkAll: e.target.checked })
   }
 
   getList(){
@@ -148,10 +246,12 @@ class Report extends Component {
       )
     }
     const Table = ({ items }) => (
-      <div className="wrap col-sm-12" style={{ marginTop: 40, maxHeight: 500, overflowY: 'scroll', overflowX: 'hidden', paddingRight: 10 }}>
+      <div>
+      <div className="wrap col-sm-12" style={{ marginTop: 40, maxHeight: 500, overflowY: 'scroll', overflowX: 'scroll', paddingRight: 10 }}>
         <table id="table-data" className="table table-striped">
           <thead>
             <tr>
+              <th><input type="checkbox" checked={this.state.checkAll} onChange={(e) => this.checkAll(e)} /></th>
               <th>Name</th>
               <th>Email</th>
               <th>Company</th>
@@ -165,6 +265,7 @@ class Report extends Component {
               <th>Score</th>
               <th>Pass</th>
               <th>License Number</th>
+              <th>Certificate</th>
             </tr>
           </thead>
           <tbody>
@@ -172,6 +273,7 @@ class Report extends Component {
             items.length ?
               items.map((item, i) => {
                 return (<tr key={i}>
+                  <td><input type="checkbox" id={i} checked={items[i].checked} onChange={(e) => this.handleChangeChecked(e, item)} /></td>
                   <td>{item.name}</td>
                   <td>{item.email}</td>
                   <td>{item.training_company}</td>
@@ -185,13 +287,16 @@ class Report extends Component {
                   <td>{item.score}</td>
                   <td>{item.pass ? 'Yes' : 'No'}</td>
                   <td>{item.license_number}</td>
+                  <td><a href={item.certificate} target="_blank">{item.certificate}</a></td>
                 </tr>)
               })
               :
-              <tr><td colspan='13'>No Data</td></tr>
+              <tr><td colspan='15'>No Data</td></tr>
             }
           </tbody>
         </table>
+      </div>
+      <Button className="btn btn-icademy-primary btn-12" onClick={this.modalSertifikat.bind(this)}>Create Certificate</Button>
       </div>
     );
     return(
@@ -221,21 +326,21 @@ class Report extends Component {
                                                             <div className="row">
                                                                 <div className="form-field-top-label">
                                                                     <label for="start">Start Date</label>
-                                                                    <DatePicker dateFormat="dd-MM-yyyy" selected={this.state.start} onChange={e => this.handleChange('start', e)} />
+                                                                    <DatePicker dateFormat="dd-MM-yyyy" selected={this.state.start} onChange={e => this.handleChangeFilter('start', e)} />
                                                                 </div>
                                                                 <div className="form-field-top-label">
                                                                     <label for="end">End Date</label>
-                                                                    <DatePicker dateFormat="dd-MM-yyyy" selected={this.state.end} onChange={e => this.handleChange('end', e)} />
+                                                                    <DatePicker dateFormat="dd-MM-yyyy" selected={this.state.end} onChange={e => this.handleChangeFilter('end', e)} />
                                                                 </div>
                                                                 <div className="form-field-top-label" style={{width:300}}>
                                                                     <label for="course">License Type</label>
-                                                                    <MultiSelect id="course" options={this.state.optionsLicensesType} value={this.state.valueLicensesType} onChange={e => this.handleChange('valueLicensesType', e)} mode="tags" enableSearch={true} resetable={true} valuePlaceholder="Select Course" />
+                                                                    <MultiSelect id="course" options={this.state.optionsLicensesType} value={this.state.valueLicensesType} onChange={e => this.handleChangeFilter('valueLicensesType', e)} mode="tags" enableSearch={true} resetable={true} valuePlaceholder="Select Course" />
                                                                 </div>
                                                                 {
                                                                     Storage.get('user').data.level !== 'client' ?
                                                                     <div className="form-field-top-label" style={{width:300}}>
                                                                         <label for="company">Company</label>
-                                                                        <MultiSelect id="company" options={this.state.optionsCompany} value={this.state.valueCompany} onChange={e => this.handleChange('valueCompany', e)} mode="tags" enableSearch={true} resetable={true} valuePlaceholder="Select Company" />
+                                                                        <MultiSelect id="company" options={this.state.optionsCompany} value={this.state.valueCompany} onChange={e => this.handleChangeFilter('valueCompany', e)} mode="tags" enableSearch={true} resetable={true} valuePlaceholder="Select Company" />
                                                                     </div>
                                                                     : null
                                                                 }
@@ -270,6 +375,59 @@ class Report extends Component {
                     </div>
                 </div>
             </div>
+          <Modal
+            show={this.state.isModalSertifikat}
+            onHide={this.handleModal}
+            size="lg"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title className="text-c-purple3 f-w-bold" style={{ color: '#00478C' }}>
+                Create Certificate
+          </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group>
+                  <div style={{ width: '750px', padding: '10px', textAlign: 'center', border: '3px solid #787878', fontSize: '25px' }}>
+                    <div style={{ width: '724px', padding: '10px', textAlign: 'center', border: '1px solid #787878' }}><br />
+
+                      <label for='cert_logo' style={{ display: 'block' }}>
+                        <img style={{ height: '50px', cursor: 'pointer' }} src={this.state.cert_logo == '' || this.state.cert_logo == null ? this.state.companyLogo : typeof this.state.cert_logo === 'object' && this.state.cert_logo !== null ? URL.createObjectURL(this.state.cert_logo) : this.state.cert_logo} />
+                      </label>
+                      <input type="file" style={{ display: 'none', cursor: 'pointer' }} id="cert_logo" name="cert_logo" onChange={this.handleChange} className="ml-5 btn btn-sm btn-default" />
+                      <span style={{ fontSize: '20px', fontWeight: 'bold' }}>
+                        <input type='text' name='cert_title' onChange={this.handleChangeText} style={{ width: '80%', border: 'none', borderBottom: '1px dashed #CCC', textAlign: 'center', fontWeight: 'bold' }} value={this.state.cert_title} />
+                      </span>
+                      <br /><br />
+                      <span style={{ fontSize: '15px' }}>
+                        <input type='text' onChange={this.handleChangeText} name='cert_subtitle' style={{ width: '80%', border: 'none', borderBottom: '1px dashed #CCC', textAlign: 'center' }} value={this.state.cert_subtitle} />
+                      </span>
+                      <br /><br />
+                      <span style={{ fontSize: '20px' }}><b>[Name]</b></span><br /><br />
+                      <span style={{ fontSize: '15px' }}>
+                        <input type='text' onChange={this.handleChangeText} name='cert_description' style={{ width: '80%', border: 'none', borderBottom: '1px dashed #CCC', textAlign: 'center' }} value={this.state.cert_description} />
+                      </span> <br /><br />
+                      <span style={{ fontSize: '18px' }}><b>[Exam - Course Topic]</b></span> <br /><br />
+                      <span style={{ fontSize: '10px' }}>{this.state.tanggal}</span><br />
+                      <label for='signature' style={{ display: 'block', cursor: 'pointer' }}>
+                        <img style={{ height: '80px' }} src={this.state.signature == '' || this.state.signature == null ? `/newasset/imginput.png` : typeof this.state.signature === 'object' && this.state.signature !== null ? URL.createObjectURL(this.state.signature) : this.state.signature} />
+                      </label>
+                      <input type="file" style={{ display: 'none', cursor: 'pointer' }} id="signature" name="signature" onChange={this.handleChange} className="ml-5 btn btn-sm btn-default" />
+                      <span style={{ fontSize: '12px' }}>
+                        <input type='text' onChange={this.handleChangeText} name='cert_sign_name' style={{ width: '80%', border: 'none', borderBottom: '1px dashed #CCC', textAlign: 'center' }} placeholder='Nama Tanda Tangan' value={this.state.cert_sign_name} />
+                      </span>
+                    </div>
+                  </div>
+                </Form.Group>
+              </Form>
+
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="btn btn-icademy-primary" onClick={() => this.sertifikat()}>
+                Send Certificate to Participant's Email
+          </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
     )
   }
