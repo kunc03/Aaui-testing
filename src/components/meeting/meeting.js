811263a572d8058c05866d20cdaef589935b3559
Alvin <alvinkurniakamal@gmail.com>
2021-06-15 04:10:25 +0700

    wording option bbb to icademy

--- src/components/meeting/meeting.js
+++ src/components/meeting/meeting.js
@@ -1478,7 +1478,7 @@ class MeetingTable extends Component {
                 <div className="col-sm-6">
                   <Form.Label className="f-w-bold">Engine</Form.Label>
                   <select value={this.state.engine} onChange={e => this.setState({ engine: e.target.value })} name="engine" className="form-control">
-                    <option value="bbb">BigBlueButton</option>
+                    <option value="bbb">ICADEMY</option>
                     <option value="zoom">Zoom</option>
                   </select>
                   <Form.Text className="text-muted">
