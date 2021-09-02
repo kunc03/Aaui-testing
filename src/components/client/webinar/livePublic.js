661c3799efa3b1e31a58ea329178619618585d16
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-09-02 10:08:47 +0700

    tambah state session jika user sudah login atau tidak

--- src/components/client/webinar/livePublic.js
+++ src/components/client/webinar/livePublic.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import { Fragment } from 'react';
 import WebinarLive from './live';
+import Storage from '../../../repository/storage';
+
 
 export default class WebinarLivePublic extends Component {
 
 	state = {
+		session: Storage.get('user').data
   }
 
 	render() {
+		const { session } = this.state
+
 		return (
-            <WebinarLive webinarId={this.props.match.params.webinarId} voucher={this.props.match.params.voucher}/>
+			<Fragment>
+				{
+					session ?
+						<Fragment>
+							<div className="pcoded-main-container">
+								<div className="pcoded-wrapper">
+									<div className="pcoded-content" style={{padding: '20px 20px 0 20px'}}>
+										<div className="pcoded-inner-content">
+											<div className="main-body">
+												<div className="page-wrapper">
+													<WebinarLive webinarId={this.props.match.params.webinarId} voucher={this.props.match.params.voucher} />
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</Fragment>
+					:
+					<WebinarLive webinarId={this.props.match.params.webinarId} voucher={this.props.match.params.voucher}/>
+				}
+			</Fragment>
 		);
 	}
 
