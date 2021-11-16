7bead19da2b4a77e9e73d1b21a35ef99de43d3bf
Alvin <alvinkurniakamal@gmail.com>
2021-11-16 15:00:02 +0700

    ktp mandatory aaui

--- src/components/training/user/form.js
+++ src/components/training/user/form.js
@@ -74,7 +74,7 @@ class FormUser extends Component {
   save = (e) => {
     this.setState({isSaving: true})
     e.preventDefault();
-    if ((!this.props.match.params.id && !this.state.expired && this.state.license_number) || !this.state.name || !this.state.born_date || !this.state.gender || !this.state.address || !this.state.city || !this.state.phone || !this.state.email || !this.state.training_company_id){
+    if ((!this.props.match.params.id && !this.state.expired && this.state.license_number) || !this.state.identity || !this.state.imageIdentity || !this.state.name || !this.state.born_date || !this.state.gender || !this.state.address || !this.state.city || !this.state.phone || !this.state.email || !this.state.training_company_id){
         toast.warning('Some field is required, please check your data.')
         this.setState({isSaving: false})
     }
@@ -477,7 +477,7 @@ class FormUser extends Component {
                                                     </div>
                                                     <div className="row">
                                                         <div className="form-field-top-label">
-                                                            <label for="imageIdentity">Identity Card Photo</label>
+                                                            <label for="imageIdentity">Identity Card Photo<required>*</required></label>
                                                             <center>
                                                                 <label style={{cursor:'pointer', borderRadius:'4px', overflow:'hidden'}}>
                                                                     <a href={this.state.imageIdentityPreview} target="_blank">
