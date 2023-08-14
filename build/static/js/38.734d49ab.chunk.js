83e42bb1dbd660a2efb931c1261c581b35ade80e
daffahan <daffa.hanifisyafiq@talenavi.com>
2023-08-14 17:36:44 +0700

    feat: [DF] push build

---
+++ build/static/js/38.734d49ab.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[38],{317:function(p,n,c){},481:function(p,n,c){},778:function(p,n,c){}}]);
+//# sourceMappingURL=38.734d49ab.chunk.js.map
